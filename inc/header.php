c8b64a38a1f4669d008122c78fa595365c62de55
Daniel Moura <daniel.mouraa@gmail.com>
2013-05-09 21:26:17 -0300

    Criacao interface da estrutura e inicio de estilizacao da home

--- inc/header.php
+++ inc/header.php
@@ -15,5 +15,20 @@
 
 		<body data-role="<?php if( isset($data_page) && $data_page == "home" ) { echo "home"; } else { echo "internas"; } ?>" data-page="<?php if(isset($data_page)) { echo $data_page; } ?>">
 		<header>
-			
+			<div class="logo">
+				<h1><a href="#this"><img src="images/lgo/cfh_esportes.png" alt="CFH Esportes" /></a></h1>
+				<span class="slogan">Este clube também é seu... Faça dele o melhor!</span>
+			</div>
+			<div class="contato">
+				<a href="#this">Fale com a gente</a>
+				<a href="#this">Trabalhe conosco</a>
+			</div>
+			<nav id="menu">
+				<ul>
+					<li><a href="#this">Quem Somos</a></li>
+					<li><a href="#this">Modalidades</a></li>
+					<li><a href="#this">Agenda de eventos</a></li>
+					<li><a href="#this">Cadastro de atletas</a></li>
+				</ul>
+			</nav>
 		</header>
--- index.php
+++ index.php
@@ -4,7 +4,116 @@
 ?>
 
 		<main>
-			<a href="#this" class="abrir-modal" data-modal="exemplo">Abrir Modal Regulamento</a>
+			<section class="destaque-home">
+				<ul>
+					<li><a href="#this"><img src="images/dtq/destaque-01.png" alt="" /></a></li>
+					<li><a href="#this"><img src="images/dtq/destaque-01.png" alt="" /></a></li>
+				</ul>
+				<nav class="setas">
+					
+				</nav>
+				<nav class="paginacao">
+					<ul>
+						<li><a href="#this">1</a></li>
+						<li><a href="#this">2</a></li>
+						<li><a href="#this">3</a></li>
+						<li><a href="#this">4</a></li>
+					</ul>
+				</nav>
+			</section>
+			<section class="conteudo">
+				<section>
+					<div class="destaque">
+						<h2>Campeonato de futsal 2013</h2>
+						<div class="thumb">
+							<img src="images/fke/1.jpg" height="224" width="354" alt="" />
+							<a href="#this" class="botao_a">Inscrições aqui!</a>
+						</div>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu arcu ac erat fermentum luctus in in turpis. </p>
+					</div>
+					<h3>Outros eventos</h3>
+					<ul class="listagem-posts">
+						<li>
+							<div class="thumb">
+								<a href="#this"><img src="images/fke/2.jpg" height="84" width="115" alt="" /></a>
+							</div>
+							<h4><a href="#this">Apresentaçao do jardim 1</a></h4>
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+						<li>
+							<div class="thumb">
+								<a href="#this"><img src="images/fke/3.jpg" height="84" width="115" alt="" /></a>
+							</div>
+							<h4><a href="#this">Campeonato de Basquete 2012</a></h4>
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+						<li>
+							<div class="thumb">
+								<a href="#this"><img src="images/fke/4.jpg" height="84" width="115" alt="" /></a>
+							</div>
+							<h4><a href="#this">Street dance</a></h4>
+							<p>Lorem ipsum dolor sit amet</p>
+						</li>
+					</ul>
+					<a href="#this" class="todos-eventos">Lista completa de eventos</a>
+				</section>
+				<aside>
+					<h2>Próximos eventos:</h2>
+					<section id="agenda">
+						<ul>
+							<li class="anterior">
+								<span class="data"><strong>23</strong>/12/2012</span>
+								<span>Campeonato lorem ipsum</span>
+							</li>
+							<li class="ativo">
+								<span class="data"><strong>23</strong>/12/2012</span>
+								<span><strong>Campeonato lorem ipsum</strong></span>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu arcu ac.</p>
+								<a href="#this" class="botao_a">Inscreva-se já!</a>
+							</li>
+							<li class="proximo">
+								<span class="data"><strong>05</strong>/02/2013</span>
+								<span>Campeonato lorem ipsum</span>
+							</li>
+						</ul>
+					</section>
+					<div class="post">
+						<div class="post-destaque">
+							<div>
+								<p><strong>Os benefícios da caoeira na formação</strong></p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu arcu ac erat fermentum luctus in in turpis. </p>
+							</div>
+						</div>
+						<ul class="listagem-posts">
+							<li>
+								<div class="thumb">
+									<a href="#this"><img src="images/fke/4.jpg" height="84" width="115" alt="" /></a>
+								</div>
+								<h4><a href="#this">Street dance</a></h4>
+								<p>Lorem ipsum dolor sit amet</p>
+							</li>
+							<li>
+								<div class="thumb">
+									<a href="#this"><img src="images/fke/4.jpg" height="84" width="115" alt="" /></a>
+								</div>
+								<h4><a href="#this">Street dance</a></h4>
+								<p>Lorem ipsum dolor sit amet</p>
+							</li>
+							<li>
+								<div class="thumb">
+									<a href="#this"><img src="images/fke/4.jpg" height="84" width="115" alt="" /></a>
+								</div>
+								<h4><a href="#this">Street dance</a></h4>
+								<p>Lorem ipsum dolor sit amet</p>
+							</li>
+						</ul>
+						<div class="banner">
+							<a href="#this"><img src="images/fke/banner.png" height="205" width="224" alt="" /></a>
+						</div>
+					</div>
+				</aside>
+			</section>
+			<!-- <a href="#this" class="abrir-modal" data-modal="exemplo">Abrir Modal Regulamento</a> -->
 		</main>
 
 <?php include("inc/footer.php"); ?>
