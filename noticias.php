c081e590bb02080274d298576201261e3b4a4102
Daniel Moura <daniel.mouraa@gmail.com>
2013-05-13 09:32:02 -0300

    Inicio criacao pagina noticias. Novas Fontes

---
+++ noticias.php
@@ -0,0 +1,63 @@
+<?php 
+	$data_page = 'noticias';
+	include("inc/header.php");
+?>
+
+		<main>
+			<section class="conteudo">
+				<section class="post">
+					<article>
+						<h1>Notícias</h1>
+						<div class="listagem-posts">
+							<ul>
+								<li>
+									<figure class="thumb">
+										<a href="#this"><img src="images/fke/4.jpg" alt=""></a>
+									</figure>
+									<h2><a href="#this">Lorem Ipsum Dolor Sit Amet</a></h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi mi, bibendum non facilisis sit amet, malesuada nec nibh. Vestibulum elementum urna a magna dignissim rhoncus.</p>
+									<a href="#this" class="ler-mais">Confira tudo a respeito</a>
+								</li>
+								<li>
+									<figure class="thumb">
+										<a href="#this"><img src="images/fke/4.jpg" alt=""></a>
+									</figure>
+									<h2><a href="#this">Lorem Ipsum Dolor Sit Amet</a></h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi mi, bibendum non facilisis sit amet, malesuada nec nibh. Vestibulum elementum urna a magna dignissim rhoncus.</p>
+									<a href="#this" class="ler-mais">Confira tudo a respeito</a>
+								</li>
+								<li>
+									<figure class="thumb">
+										<a href="#this"><img src="images/fke/4.jpg" alt=""></a>
+									</figure>
+									<h2><a href="#this">Lorem Ipsum Dolor Sit Amet</a></h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi mi, bibendum non facilisis sit amet, malesuada nec nibh. Vestibulum elementum urna a magna dignissim rhoncus.</p>
+									<a href="#this" class="ler-mais">Confira tudo a respeito</a>
+								</li>
+								<li>
+									<figure class="thumb">
+										<a href="#this"><img src="images/fke/4.jpg" alt=""></a>
+									</figure>
+									<h2><a href="#this">Lorem Ipsum Dolor Sit Amet</a></h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi mi, bibendum non facilisis sit amet, malesuada nec nibh. Vestibulum elementum urna a magna dignissim rhoncus.</p>
+									<a href="#this" class="ler-mais">Confira tudo a respeito</a>
+								</li>
+								<li>
+									<figure class="thumb">
+										<a href="#this"><img src="images/fke/4.jpg" alt=""></a>
+									</figure>
+									<h2><a href="#this">Lorem Ipsum Dolor Sit Amet</a></h2>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas mi mi, bibendum non facilisis sit amet, malesuada nec nibh. Vestibulum elementum urna a magna dignissim rhoncus.</p>
+									<a href="#this" class="ler-mais">Confira tudo a respeito</a>
+								</li>
+							</ul>
+						</div>
+					</article>
+					<aside id="sidebar">
+
+					</aside>
+				</section>
+			</section>
+		</main>
+
+<?php include("inc/footer.php"); ?>
